5ba8d2d5bb47faef39b865ecbda165748ee93b03
Sebastian Hong <kxhong.2018@scis.smu.edu.sg>
2025-03-18 14:32:50 +0800

    st PV tier

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("hello world")
+
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
+
+# df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_excel('BNEF Tier PV.xlsx')  # will work for Excel files
+
+st.title("BNEF Tier PV")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
